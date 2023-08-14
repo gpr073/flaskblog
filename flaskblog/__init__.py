ac6b1eabfe86a6dfa46d9f58d2d16c89d2d79dd7
gpr073 <lonewolf41320@gmail.com>
2023-08-15 00:59:14 +0530

    Added validation and setup login and register routes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 #Need to make this environment variable before committing.
 app.config['SECRET_KEY'] = '0f615fc37299e4d68ad5624ed7278ecd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
-
-from flaskblog import routes
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,32 +1,43 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField, validators
-#from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', [
-            validators.InputRequired(), 
-            validators.Length(min=2, max=20)
+            InputRequired(), 
+            Length(min=2, max=20)
         ])
     email = EmailField('Email', [
-            validators.InputRequired(), 
-            validators.Email()
+            InputRequired(), 
+            Email()
         ])
     password = PasswordField('Password', [
-            validators.InputRequired(), 
-            validators.Length(min=5, max=20)
+            InputRequired(), 
+            Length(min=5, max=20)
         ])
     confirm_password = PasswordField('Confirm_Password', [
-            validators.InputRequired(), 
-            validators.Length(min=5, max=20), 
-            validators.EqualTo('password', message="Passwords must match")
+            InputRequired(), 
+            Length(min=5, max=20), 
+            EqualTo('password', message="Passwords must match")
         ])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('An account already exists with this email id.')
+
 class LoginForm(FlaskForm):
     email = EmailField('Email', [
-            validators.InputRequired(), 
-            validators.Email()
+            InputRequired(), 
+            Email()
         ])
-    password = PasswordField('Password', [validators.InputRequired()])
+    password = PasswordField('Password', [InputRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
